280797e8dace043c984261d48d6401fa453a9c1f
kermekx <kermekx@gmail.com>
2015-12-21 00:11:20 +0100

    Fullscreen mode
    added fullscreen using F5

--- KermekxEngineOpenGL/src/com/kermekx/engine/KermekxEngine.java
+++ KermekxEngineOpenGL/src/com/kermekx/engine/KermekxEngine.java
@@ -21,7 +21,7 @@ public abstract class KermekxEngine implements Runnable {
 	private final String WINDOW_NAME;
 	private final int WIDTH;
 	private final int HEIGHT;
-	private boolean fullScren = false;
+	private boolean fullScreen = false;
 	private Renderer renderer = new Renderer();
 
 	private long lastFrame;
@@ -69,7 +69,7 @@ public abstract class KermekxEngine implements Runnable {
 
 		while (!Display.isCloseRequested()) {
 			if (Keyboard.isKeyDown(Key.KEY_F5))
-				this.setFullScreen(!fullScren);
+				this.setFullScreen(!fullScreen);
 
 			int delta = getDelta();
 			renderer.update(delta);
@@ -85,10 +85,76 @@ public abstract class KermekxEngine implements Runnable {
 	}
 
 	private void setFullScreen(boolean fullScreen) {
-		/*
-		 * try { Display.setFullscreen(fullScreen); fullScren = !fullScren; }
-		 * catch (LWJGLException e) { e.printStackTrace(); }
-		 */
+		try {
+			if (fullScreen)
+				setDisplayMode(WIDTH, HEIGHT, fullScreen);
+			else
+				setDisplayMode(WIDTH / 2, HEIGHT / 2, fullScreen);
+			Display.setFullscreen(fullScreen);
+			this.fullScreen = fullScreen;
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Set the display mode to be used
+	 * 
+	 * @param width
+	 *            The width of the display required
+	 * @param height
+	 *            The height of the display required
+	 * @param fullscreen
+	 *            True if we want fullscreen mode
+	 */
+	public void setDisplayMode(int width, int height, boolean fullscreen) {
+
+		if ((Display.getDisplayMode().getWidth() == width) && (Display.getDisplayMode().getHeight() == height)
+				&& (Display.isFullscreen() == fullscreen)) {
+			return;
+		}
+
+		try {
+			DisplayMode targetDisplayMode = null;
+
+			if (fullscreen) {
+				DisplayMode[] modes = Display.getAvailableDisplayModes();
+				int freq = 0;
+
+				for (int i = 0; i < modes.length; i++) {
+					DisplayMode current = modes[i];
+
+					if ((current.getWidth() == width) && (current.getHeight() == height)) {
+						if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+							if ((targetDisplayMode == null)
+									|| (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+								targetDisplayMode = current;
+								freq = targetDisplayMode.getFrequency();
+							}
+						}
+
+						if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel())
+								&& (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+							targetDisplayMode = current;
+							break;
+						}
+					}
+				}
+			} else {
+				targetDisplayMode = new DisplayMode(width, height);
+			}
+
+			if (targetDisplayMode == null) {
+				System.out.println("Failed to find value mode: " + width + "x" + height + " fs=" + fullscreen);
+				return;
+			}
+
+			Display.setDisplayMode(targetDisplayMode);
+			Display.setFullscreen(fullscreen);
+
+		} catch (LWJGLException e) {
+			KELogger.logInfo("Unable to setup mode " + width + "x" + height + " fullscreen=" + fullscreen + e);
+		}
 	}
 
 	public int getDelta() {
