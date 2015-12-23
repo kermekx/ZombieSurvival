ced5d01c453ced04eb1dfc88b2d4e899ef4aabea
kermekx <kermekx@gmail.com>
2015-12-23 14:57:38 +0100

    Ajouter et retirer des items du sac

--- ZombieSurvival/src/com/kermekx/zombiesurvival/inventory/Inventory.java
+++ ZombieSurvival/src/com/kermekx/zombiesurvival/inventory/Inventory.java
@@ -1,5 +1,39 @@
 package com.kermekx.zombiesurvival.inventory;
 
 public class Inventory {
+	
+	private final ItemStack[] items;
+	
+	public Inventory(int size) {
+		items = new ItemStack[size];
+	}
+	
+	public ItemStack addItem(ItemStack is) {
+		int i = 0;
+		int amount = is.getAmount();
+		while(amount > 0 && i < items.length) {
+			if(items[i] == null) {
+				items[i] = is;
+			} else if (items[i].getItemId() == is.getItemId()) {
+				amount = items[i].add(amount);
+				is.setAmount(amount);
+			}
+			i++;
+		}
+		return (amount == 0) ? null : is;
+	}
+	
+	public ItemStack removeItem(ItemStack is) {
+		int i = 0;
+		int amount = is.getAmount();
+		while(amount > 0 && i < items.length) {
+			if (items[i].getItemId() == is.getItemId()) {
+				amount = items[i].remove(amount);
+				is.setAmount(amount);
+			}
+			i++;
+		}
+		return (amount == 0) ? null : is;
+	}
 
 }
--- ZombieSurvival/src/com/kermekx/zombiesurvival/inventory/ItemStack.java
+++ ZombieSurvival/src/com/kermekx/zombiesurvival/inventory/ItemStack.java
@@ -3,23 +3,57 @@ package com.kermekx.zombiesurvival.inventory;
 import com.kermekx.zombiesurvival.item.Item;
 
 public class ItemStack {
-	
+
+	public static final int maxStack = 99;
+
 	private final int itemId;
 	private int amount = 1;
-	
+
 	public ItemStack(int id) {
 		itemId = id;
 	}
-	
+
 	public ItemStack(int id, int amount) {
 		itemId = id;
-		this.amount = amount;
+		this.amount = (!getItem().isStackable()) ? 1 : (amount > maxStack) ? maxStack : amount;
 	}
 	
+	public int getItemId() {
+		return itemId;
+	}
+
 	public Item getItem() {
 		return Item.items[itemId];
 	}
 	
+	public int getAmount() {
+		return amount;
+	}
 	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 
+	public int add(int amount) {
+		if (!getItem().isStackable())
+			return amount;
+		this.amount = this.amount + amount;
+		if(this.amount <= maxStack)
+			return 0;
+		int left = this.amount - maxStack;
+		this.amount = maxStack;
+		return left;
+	}
+	
+	public int remove(int amount) {
+		if (this.amount > amount) {
+			this.amount -= amount;
+			return 0;
+		} else {
+			int left = amount - this.amount;
+			this.amount = 0;
+			return left;
+		}
+	}
+	
 }
