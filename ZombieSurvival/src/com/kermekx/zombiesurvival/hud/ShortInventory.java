bab6e0ec7ae726af4da42f45418025359f35e7e7
kermekx <kermekx@gmail.com>
2015-12-23 22:42:14 +0100

    HUD affiche les 9 premiers items du sac

--- ZombieSurvival/src/com/kermekx/zombiesurvival/hud/ShortInventory.java
+++ ZombieSurvival/src/com/kermekx/zombiesurvival/hud/ShortInventory.java
@@ -10,22 +10,29 @@ import com.kermekx.engine.drawable.list.DisplayList;
 import com.kermekx.engine.hud.HUD;
 import com.kermekx.engine.position.Vector;
 import com.kermekx.zombiesurvival.inventory.Inventory;
+import com.kermekx.zombiesurvival.inventory.ItemStack;
+import com.kermekx.zombiesurvival.item.Item;
 
 public class ShortInventory extends HUD {
-	
+
 	public ShortInventory(Inventory inventory) {
 		List<Drawable> drawables = new ArrayList<Drawable>();
-		drawables.add(new Rectangle2D(960, 975, 960, 10, ColorBuilder.WHITE));
-		drawables.add(new Rectangle2D(960, 1065, 960, 10, ColorBuilder.WHITE));
+		drawables.add(new Rectangle2D(960, 975, 960, 10, ColorBuilder.BLACK));
+		drawables.add(new Rectangle2D(960, 1065, 960, 10, ColorBuilder.BLACK));
 		
 		for (int i = 0; i < 11; i ++)
-			drawables.add(new Rectangle2D(480 + 96 * i, 1020, 10, 100, ColorBuilder.WHITE));
+			drawables.add(new Rectangle2D(480 + 96 * i, 1020, 10, 100, ColorBuilder.BLACK));
 		addDisplayList(new DisplayList(drawables, new Vector(0, 0), new Vector(1920, 1080)));
 		
 		for (int i = 0; i < 10; i ++)
-			addDrawable(new Rectangle2D(528 + 96 * i, 1020, 86, 80, ColorBuilder.RED));
+			addDrawable(new Rectangle2D(528 + 96 * i, 1020, 86, 80, 0));
+		
+		for (int i = 0; i < 10; i ++) {
+			ItemStack is = inventory.getSlot(i);
+			
+			if(is != null)
+				getDrawables().get(i).setTexture(Item.items[is.getItemId()].getTextureID());
+		}
 	}
-	
-	
 
 }
